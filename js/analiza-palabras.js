0a3d5045a837a775d0d08ea9e4e34cbd62d55a01
Jonatan Sánchez <jona2255@gmail.com>
2021-02-19 21:08:25 +0100

    Javascript terminado.

--- js/analiza-palabras.js
+++ js/analiza-palabras.js
@@ -1,4 +1,22 @@
-/* Crea tu función aquí */
 function analizaPalabras(palabras) {
 
+    for (const [i, palabra] of palabras.entries()) {
+        console.log("######################")
+        console.log(`Palabra ${i}: ${palabra}`);
+        console.log(`Nº de caracteres: ${palabra.length}`);
+
+        const letras = palabra.split("").reduce((acc, value) => acc + "-" + value);
+        console.log(`Deletreo: ${letras}`);
+
+        if (palabra.length % 2 === 0) {
+            console.log("La palabra es par y no empieza por mayúscula");
+        } else {
+            console.log("La palabra es impar y empieza por mayúscula");
+        }
+        if (palabra.includes("ble")) {
+            console.log("La palabra contiene 'ble'");
+        } else {
+            console.log("La palabra no contiene 'ble'");
+        }
+    }
 };
